8f8b8da528803d054e72363a466337788192b307
mloeff <michaelloeffler23@gmail.com>
2023-04-28 22:35:09 -0500

    sets up index and typeDefs in the Schemas folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: String
+    user: User
+  }
+
+  type Query {
+    me: [User]
+  }
+
+  input SaveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+    bookId: Int!
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(criteria: SaveBookInput): User
+    removebook(bookId: Int): User
+  }
+`;
+
+module.exports = typeDefs;
